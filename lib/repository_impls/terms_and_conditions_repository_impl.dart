57b8178ecc081f835dab7dbdeaa0ec13857177f2
vxplore <vik@v-xplore.com>
2022-11-26 11:33:00 +0530

    26.11.2022 11:32

--- lib/repository_impls/terms_and_conditions_repository_impl.dart
+++ lib/repository_impls/terms_and_conditions_repository_impl.dart
@@ -8,7 +8,8 @@ class TermsAndContionsRepositoryImpl extends TermsAndConditionsRepository{
   @override
   Future<Resource<RequiredDataTermsAndConditionsResponse>> getTermsAndConditionsInittialData(String userId) async {
     await Future.delayed(const Duration(seconds: 2));
-    return Success(RequiredDataTermsAndConditionsResponse(status: true, message: "Success", termsConditionsTxt: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."));
+    return Success(RequiredDataTermsAndConditionsResponse(status: true, message: "Success",
+        termsConditionsTxt: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."));
   }
 
 }
